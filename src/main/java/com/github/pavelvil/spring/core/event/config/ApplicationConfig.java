1914b01d97e3da5de6cdde5f379985612da9748b
PavelVil <pavel.vavilov.1994@gmail.com>
2023-12-30 19:12:49 +0300

    Event Listener examples

--- src/main/java/com/github/pavelvil/spring/core/event/config/ApplicationConfig.java
+++ src/main/java/com/github/pavelvil/spring/core/event/config/ApplicationConfig.java
@@ -3,6 +3,9 @@ package com.github.pavelvil.spring.core.event.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -14,6 +17,21 @@ public class ApplicationConfig {
     @Bean
     public BlockingQueue<String> orderQueue() {
         return new LinkedBlockingQueue<>();
+    }
+
+    @Bean
+    public ApplicationEventMulticaster applicationEventMulticaster() {
+        SimpleApplicationEventMulticaster eventMulticaster =
+                new SimpleApplicationEventMulticaster();
+
+        SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor();
+        executor.setTaskDecorator(runnable -> {
+            System.out.println("Call task from executor");
+            return runnable;
+        });
+
+        eventMulticaster.setTaskExecutor(executor);
+        return eventMulticaster;
     }
 
 }
